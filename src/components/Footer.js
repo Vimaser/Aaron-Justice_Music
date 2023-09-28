9337531dbb22788822d83eedbdd0be5aece5638a
Vimaser <thfeske@gmail.com>
2023-09-28 17:42:32 -0500

    added admin portal link in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./css/Footer.css";
 
 const Footer = () => {
@@ -38,6 +39,9 @@ const Footer = () => {
         >
           YouTube
         </a>
+        <Link to="/admin" className="social-link">
+          Admin Portal
+        </Link>
       </div>
     </footer>
   );
