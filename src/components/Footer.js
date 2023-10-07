5acdc65e4bb4144a9534324855be65f6212dd3da
Vimaser <thfeske@gmail.com>
2023-10-07 18:28:46 -0500

    added more social media links plus minor css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,42 +6,54 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>© 2023 Aaron Justice Music</p>
-      <div className="social-links">
-        <a
-          className="social-link"
-          href="https://www.facebook.com/AaronJusticeMusic"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Facebook
-        </a>
-        <a
-          className="social-link"
-          href="https://www.instagram.com/aaronjusticemusic"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Instagram
-        </a>
-        <a
-          className="social-link"
-          href="https://www.twitter.com/aaronjusticemusic"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Twitter
-        </a>
-        <a
-          className="social-link"
-          href="https://www.youtube.com/aaronjusticemusic"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          YouTube
-        </a>
-        <Link to="/admin" className="social-link">
-          Admin Portal
-        </Link>
+      <div className="social-links-container">
+        <div className="social-links-1">
+          <a
+            className="social-link"
+            href="https://www.facebook.com/AaronJusticeMusic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </a>
+          <a
+            className="social-link"
+            href="https://www.instagram.com/aaronjusticemusic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            className="social-link"
+            href="https://open.spotify.com/artist/6umNxjlFms7UDoZOlZBpOv?fbclid=IwAR2Yqjl-3ILpwEmVwMQV8CokedkNRP0OHLvhm0-EMYExdHUgudauOZ1Po5w"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Spotify
+          </a>
+        </div>
+        <div className="social-links-2">
+          <a
+            className="social-link"
+            href="https://www.youtube.com/@aaronjusticemusic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            YouTube
+          </a>
+          <a
+            className="social-link"
+            href="https://www.tiktok.com/@theaaronjustice?_t=8gGlSMm2vyF&_r=1&fbclid=IwAR1HVTO_FuCRbKqnZqWcQSpvjf7zpMvcXpf_sNUTmrfMXkkpTeCxj7735Ww"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TikTok
+          </a>
+          <Link to="/admin" className="social-link">
+            AdminPortal
+          </Link>
+        </div>
       </div>
     </footer>
   );
